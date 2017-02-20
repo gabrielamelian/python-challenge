e915655d2cd592236295fda8ccd4ab4b750a7c0c
gabrielamelian <gabrielamelian@gmail.com>
2017-02-20 18:21:21 +1300

    Adds string module to get alphabet

---
+++ 1_challenge.py
@@ -0,0 +1,18 @@
+import string
+
+to_decrypt = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+alphabet = list(string.ascii_lowercase)
+
+# for letter in to_decrypt:
+#     if (letter == 'm'):
+#         print 'k'
+#     if (letter == 'q'):
+#         print 'o'
+#     if (letter == 'g'):
+#         print 'e'
+#     else:
+#         print letter
+
+# def decrypt(letter):
+
+print alphabet
