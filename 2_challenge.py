9f66c05d156669d3220c088a37777e80a9f803d7
gabrielamelian <gabrielamelian@gmail.com>
2017-02-20 20:45:36 +1300

    Script finds the rare characters

--- 2_challenge.py
+++ 2_challenge.py
@@ -3,5 +3,8 @@
 import collections
 
 file_to_decrypt = open("2_text.txt", "r")
+char_frequency = collections.Counter(file_to_decrypt.read())
 
-print collections.Counter(file_to_decrypt.read())
+for char, frequency in char_frequency.iteritems():
+    if (frequency == 1):
+        print char
