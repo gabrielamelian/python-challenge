ba09ca635bb4ddaa6c5781146f259c73c1e5e634
gabrielamelian <gabrielamelian@gmail.com>
2017-02-20 21:00:35 +1300

    Imports only counter from collections

--- 2_challenge.py
+++ 2_challenge.py
@@ -1,9 +1,9 @@
 """Decrypt text challenge2."""
 
-import collections
+from collections import Counter
 
 file_to_decrypt = open("2_text.txt", "r")
-char_frequency = collections.Counter(file_to_decrypt.read())
+char_frequency = Counter(file_to_decrypt.read())
 
 for char, frequency in char_frequency.iteritems():
     if (frequency == 1):
