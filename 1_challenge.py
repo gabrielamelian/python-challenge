b4b98f4df882113190cec0815fdc3d48e985dc82
gabrielamelian <gabrielamelian@gmail.com>
2017-02-20 19:05:22 +1300

    Resolves the cryptic with my own solution

--- 1_challenge.py
+++ 1_challenge.py
@@ -3,16 +3,13 @@ import string
 to_decrypt = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 alphabet = list(string.ascii_lowercase)
 
-# for letter in to_decrypt:
-#     if (letter == 'm'):
-#         print 'k'
-#     if (letter == 'q'):
-#         print 'o'
-#     if (letter == 'g'):
-#         print 'e'
-#     else:
-#         print letter
+def decrypt(letter):
+    if (letter in alphabet):
+        decoded_letter_index = alphabet.index(letter) + 2
+        max_index = len(alphabet) -1
+        if (decoded_letter_index >= max_index):
+            return alphabet[decoded_letter_index - len(alphabet)]
+        return alphabet[decoded_letter_index]
+    return letter
 
-# def decrypt(letter):
-
-print alphabet
+print ''.join(map(decrypt, to_decrypt))
